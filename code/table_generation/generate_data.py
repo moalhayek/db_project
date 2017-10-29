1424abbf7c7da7eabcc52342ab26f083d55a6e3a
Mo Alhayek <almoalhayek@gmail.com>
2017-10-29 19:18:12 -0400

    added some patterns to py file

--- code/table_generation/generate_data.py
+++ code/table_generation/generate_data.py
@@ -7,29 +7,57 @@ fake = Faker()
 def generate_name(prof):
   return prof['name']
 
-def generate_age(prof):
+def generate_age():
   return str(randint(21,60))
 
 def generate_gender(prof):
   return prof['sex']
 
-def generate_street_address(prof):
+def generate_street_address():
   return fake.street_address()
 
-def generate_city(prof):
+def generate_city():
   return fake.city()
 
-def generate_salary(prof):
-  return str(abs(int(gauss(95000,35000))))
+def generate_salary(age):
+  if (age < 25){
+    mean = 55000
+  }
+  else if (age < 35){
+    mean = 75000
+  }
+  else{
+    mean = 95000
+  }
+  return str(abs(int(gauss(mean,35000))))
 
-def generate_spending_per_night(prof):
-  return str(abs(int(gauss(50,500))))
+def generate_spending_per_night(salary):
+  if (salary < 60000){
+    mean = 25
+  }
+  else if (salary < 130000){
+    mean = 50
+  }
+  else{
+    mean = 100
+  }
+  return str(abs(int(gauss(mean,500))))
 
-def generate_crowding_pref(prof):
+def generate_crowding_pref(age):
   crowd_types = ['Not crowded','Moderately crowded','Very crowded']
-  return choice(crowd_types)
+  p = []
+  if (age < 30){
+    p = [.33, .33, .34]
+  }
+  else if (age<45){
+    p = [.5, .3, .2]
+  }
+  else{
+    p = [.7, .2, .1]
+  }
+  return choice(crowd_types, p)
 
-def generate_relationship_status(prof):
+def generate_relationship_status():
   relationship_types = ['Single','In a relationship']
   return choice(relationship_types)
  
@@ -38,18 +66,6 @@ drinker_attributes = ["name","age","gender","street_address","city","salary","sp
 
 #how many?
 amount = 15000
-
-#use a dict to map the attributes to their functions  
-options = {'name': generate_name,
-           'age': generate_age,
-           'gender': generate_gender,
-           'street_address': generate_street_address,
-           'city': generate_city,
-           'salary': generate_salary,
-           'spending_per_night': generate_spending_per_night,
-           'crowding_pref': generate_crowding_pref,
-           'relationship_status': generate_relationship_status,
-}
 
 outputfile_name = 'drinker_table.csv'
 outputfile = open(outputfile_name,'w')
@@ -61,9 +77,18 @@ for index in range(1,amount):
     # generate random profile
     #iterate through all desired attributes
     random_profile = fake.profile()
-    for attr in drinker_attributes:      
-        new_attribute =  options[attr](random_profile)    #pass in profile
-        tuple_list.append(new_attribute)
+    age = generate_age()
+    sal = generate_salary(age)
+    tuple_list.append(generate_name(random_profile))
+    tuple_list.append(age)
+    tuple_list.append(generate_gender(random_profile))
+    tuple_list.append(generate_street_address())
+    tuple_list.append(generate_city())
+    tuple_list.append(sal)
+    tuple_list.append(generate_spending_per_night(sal))
+    tuple_list.append(generate_crowding_pref(age))
+    tuple_list.append(generate_relationship_status())
+
   #format as comma_delimited string 
     csv_string = ','.join(tuple_list)
     csv_string += '\n'
