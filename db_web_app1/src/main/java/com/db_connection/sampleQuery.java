f34ed594260fdf5a35c54ca5352fec949eafa5a3
Mo Alhayek <almoalhayek@gmail.com>
2017-11-14 14:09:41 -0500

    sample query works now

--- db_web_app1/src/main/java/com/db_connection/sampleQuery.java
+++ db_web_app1/src/main/java/com/db_connection/sampleQuery.java
@@ -7,7 +7,9 @@ import java.sql.*;
 
 public class sampleQuery {
 
-    public static void sample(){
+    public static String sample(String id){
+
+        String ret = new String();
 
         try {
             //Get the database connection
@@ -18,10 +20,16 @@ public class sampleQuery {
             Statement stmt = con.createStatement();
 
             //Make a SELECT query from the table specified by the 'command' parameter at the index.jsp
-            String str = "SELECT * FROM drinkers";
+            String str = String.format("SELECT * FROM drinkers WHERE id ='%s'", id);
+
             //Run the query against the database.
             ResultSet result = stmt.executeQuery(str);
 
+            //initially, result points to before first row
+            result.next();
+
+            //now store first row results
+            ret = result.getString("name");
             while (result.next()) {
                 System.out.print(result.getString("name"));
             }
@@ -30,10 +38,12 @@ public class sampleQuery {
         } catch (Exception e) {
             System.out.print(e);
         }
+
+        return ret;
     }
 
     public static void main(String[] args) {
-        sample();
+        sample("1");
     }
 
 
