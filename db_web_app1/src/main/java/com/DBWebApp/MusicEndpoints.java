6f2c5672d2f9917eef1c5bd819a8747ae96a1e68
Brian Monticello <b.monticello23@gmail.com>
2017-11-18 17:35:23 -0500

    added ageEarnings rest endpoint

---
+++ db_web_app1/src/main/java/com/DBWebApp/MusicEndpoints.java
@@ -0,0 +1,20 @@
+package com.DBWebApp;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+import com.DatabaseConn.MusicQueries;
+import com.google.gson.Gson;
+
+@Path("/music")
+public class MusicEndpoints {
+    @Path("/getMusicTrends")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getMusicTrends(@DefaultValue("21") @QueryParam("lowerAge") String lowerAge, @DefaultValue("60") @QueryParam("upperAge") String upperAge) {
+        MusicQueries m = new MusicQueries();
+        Gson gson = new Gson();
+        String json = gson.toJson(m.getMusicTrends(lowerAge, upperAge));
+        return json;
+    }
+}
