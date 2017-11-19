fc1e6f3d9a32a7c39dd4dd1185423010fe0be872
Brian D. Monticello <b.monticello23@gmail.com>
2017-11-19 16:09:38 -0500

    ad purchases and ad platform

---
+++ code/table_generation/create_ads_purchases.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `ad_purchases` (
+  `bar_id` INT NOT NULL,
+  `platform_id` INT NULL,
+  `start_date` DATE NULL,
+  'end_date' DATE NULL,
+  'total_clicks' INT NULL,
+  PRIMARY KEY (`bar_id`));
+  
+LOAD DATA LOCAL INFILE '/Users/Brian/Documents/School/Rutgers University/Senior Year/Fall 2017/Principles of Info & Data Management/Project/db_project/code/table_generation/ad_purchases_table.csv' 
+INTO TABLE ad_purchases
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES;
--- code/table_generation/generate_ad_purchases.py
+++ code/table_generation/generate_ad_purchases.py
@@ -5,7 +5,7 @@ fake = Faker()
 outputfile_name = 'ad_purchases_table.csv'
 outputfile = open(outputfile_name,'w')
 
-attr_list = ['bar_id','platform_id','start_date','end_date','cost_per_click','cost_to_date']
+attr_list = ['bar_id','platform_id','start_date','end_date','total_clicks']
 csv_str = ','.join(attr_list) + '\n'
 outputfile.write(csv_str)
 
@@ -26,15 +26,11 @@ for bar_id in bar_list:
         date_obj = fake.date_between(start_date="-2y",end_date="today")
         end_date = '{0}-{1}-{2}'.format(str(date_obj.year),str(date_obj.month),str(date_obj.day))
 
-        mean = 1
-        sigma = 0.2
-        cost_per_click = '{0:.2f}'.format(gauss(mean,sigma))
-
         mean = 2000
         sigma = 1200
-        cost_to_date = str(abs(int(gauss(mean,sigma))))
+        clicks = str(abs(int(gauss(mean,sigma))))
 
-        csv_str = '{0},{1},{2},{3},{4},{5}\n'.format(str(bar_id),str(ad_platform),start_date,end_date,cost_per_click,cost_to_date)
+        csv_str = '{0},{1},{2},{3},{4}\n'.format(str(bar_id),str(ad_platform),start_date,end_date,clicks)
         outputfile.write(csv_str)
 
 
