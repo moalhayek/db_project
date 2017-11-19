21857391b07a0dd874ddb3087801012101ff34f7
Brian Monticello <b.monticello23@gmail.com>
2017-11-18 19:21:17 -0500

    fixed mo's bullshit error

--- db_web_app1/src/main/java/com/DBWebApp/DrinkerEndpoints.java
+++ db_web_app1/src/main/java/com/DBWebApp/DrinkerEndpoints.java
@@ -3,27 +3,22 @@ package com.DBWebApp;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import com.DatabaseConn.DrinkerQueries;
-import com.google.gson.Gson;
+import com.IDBWebApp.IDrinkerEndpoints;
 
 @Path("/drinkers")
 public class DrinkerEndpoints {
     @Path("/getDrinker")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getDrinker(@DefaultValue("1") @QueryParam("id") String id) {
+    public IDrinkerEndpoints.IDrinkerResult getDrinker(@DefaultValue("1") @QueryParam("id") String id) {
         DrinkerQueries d = new DrinkerQueries();
-        Gson gson = new Gson();
-        String json = gson.toJson(d.drinker(id));
-
-        return json;
+        return d.drinker(id);
     }
     @Path("/getAgeEarning")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getAgeEarning() {
+    public IDrinkerEndpoints.IAgeEarningsResult getAgeEarning() {
         DrinkerQueries earn = new DrinkerQueries();
-        Gson gson = new Gson();
-        String json = gson.toJson(earn.ageEarnings());
-        return json;
+        return earn.ageEarnings();
     }
 }
