e80e8b4d6c053eef62ca3add05ec2f863c8992bb
Sean Olejar <solejar236@gmail.com>
2017-11-19 23:09:51 -0500

    deleting everything temporarily

--- code/table_generation/create_music.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `music` (
-  `id` INT NOT NULL,
-  `genre` VARCHAR(200) NULL,
-  PRIMARY KEY (`id`));
-
-LOAD DATA LOCAL INFILE '/Users/Brian/Documents/School/Rutgers University/Senior Year/Fall 2017/Principles of Info & Data Management/Project/db_project/code/table_generation/music_table.csv' 
-INTO TABLE music
-FIELDS TERMINATED BY ','
-IGNORE 1 LINES;  
