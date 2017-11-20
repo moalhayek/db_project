b9375a89377e6034d7cc78295b5f3b64692d4385
Mo Alhayek <almoalhayek@gmail.com>
2017-11-20 12:29:36 -0500

    added transactions monthly earnings endpoint

--- db_web_app1/src/main/java/com/IDBWebApp/ITransactionsEndpoints.java
+++ db_web_app1/src/main/java/com/IDBWebApp/ITransactionsEndpoints.java
@@ -1,4 +1,15 @@
 package com.IDBWebApp;
 
+import java.util.List;
+
 public class ITransactionsEndpoints {
+    public class IEarning {
+        public String month;
+        public String year;
+        public int earnings;
+    }
+
+    public class IEarningsResult {
+        public List<IEarning> monthly_earnings;
+    }
 }
