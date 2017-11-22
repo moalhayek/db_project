5acfeb40bd8c5c087820f28734fe3da546426c69
Brian Monticello <b.monticello23@gmail.com>
2017-11-21 21:08:02 -0500

    ads module

--- db_web_app1/web/js/advertisementController.js
+++ db_web_app1/web/js/advertisementController.js
@@ -1,59 +1,54 @@
 var app = angular.module('db-project')
 
-//controller for personal ad purchase info
-app.controller("advertisementsController", function (sharedProperties) {
+//controller for the general ad platform info
+app.controller('advertisementController',function(sharedProperties){
 
-  this.timeType = 'monthly';  
-  this.labels = ['Foo',"January", "February", "March", "April", "May", "June", "July"];
-  this.labels_days_of_week = ['Mon','Tue','Wed','Thurs','Fri','Sat','Sun'];
-  this.series = ['Happy Hour Sales', 'Late Night Sales'];
-  this.data = [
-    [65, 59, 80, 81, 56, 55, 40.34],
-    [28, 48, 40, 19, 86, 27, 90,25]
-  ];
-  
-  this.onClick = function (points, evt) {
-    //console.log(points, evt);
-  };
 
-  this.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-  this.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
-        }
-      ]
-    }
-  };
 
-    this.setAds = function(barID){
-        //REST call
-        console.log('ads data generated with barID ' +barID);
-        
-        var results = [
-          {'id': 1,'name': 'Google','startDate': '01-02-2017','endDate': '05-07-2017','costToDate': 2567,'costPerClick': 0.64,'clicksToDate': 3145},
-          {'id': 2,'name': 'Facebook','startDate': '04-22-2016','endDate': '12-07-2016','costToDate': 2567,'costPerClick': 0.64,'clicksToDate': 3145},
-          {'id': 3,'name': 'Youtube','startDate': '11-12-2016','endDate': '11-17-2017','costToDate': 2567,'costPerClick': 0.64,'clicksToDate': 3145}
-        ];
-        sharedProperties.setProperty('advertisements',results);
-    };
+    this.labels = ['Total Clicks','Total Cost','Cost Per Click']
+    this.series = []
+    this.data = [];
 
     this.getAds = function(){
-        return sharedProperties.getProperty('advertisements');
+        return sharedProperties.getProperty('adPlatforms')
     };
-});
 
-//controller for the general ad platform info
-app.controller('adPlatformController',function(){
+    this.setAds = function(barID){
+        //REST endpoint with whichBar as param
+        var options = {
+            url: 'ads/getAdPurchases',
+            params: {
+                barId: barID,
+            },
+            method: 'GET',
+            destination: 'adPurchase'
+        };
 
+        var promise = sharedProperties.httpReq(options);
+
+        promise.then(function(res){
+            sharedProperties.setProperty('adPlatforms',res.adPurchases)
+            //console.log(sharedProperties.getProperty('adPurchases'))
+        }.bind(this));
+    };
+
+    this.data =[];
+
+    this.setData = function(platform){
+        var tempData = [];
+        //['Start Date','End Date','Total Clicks','Total Cost','Cost Per Click']
+        var dataSet = [platform['totalClicks'], platform['totalCost'], platform['costPerClick']];
+        tempData.push(dataSet);
+        this.data = tempData
+    };
+    this.getAdEarnings = function(targetMonth){
+        var arr = sharedProperties.getProperty('monthlyEarnings').monthly_earnings;
+        var obj = {}
+        arr.forEach(function(elem){
+            if(elem.month == targetMonth){
+                obj = elem;
+            }
+        });
+        return obj.total_earnings;
+    };
 });
